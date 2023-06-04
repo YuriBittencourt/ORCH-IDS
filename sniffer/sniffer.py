3d15fc945711e3dd9fcfd07be7c899ebf241ab6e
YuriBittencourt <bittencourt.yuri@gmail.com>
2023-06-04 07:17:07 -0300

    Feat: Holy shit, ultra long commit

--- sniffer/sniffer.py
+++ sniffer/sniffer.py
@@ -1,33 +1,78 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
 from scapy.all import *
-import json
+import time
 
-# Função para processar os pacotes capturados
+load_dotenv()
+
+mongo_host = os.getenv('MONGO_HOST')
+mongo_port = int(os.getenv('MONGO_PORT'))
+mongo_db = os.getenv('MONGO_DB')
+collection = os.getenv('MONGO_COLLECTION_QUEUE')
+client = MongoClient(host=mongo_host, port=mongo_port)
+
+
+db = client[mongo_db]
+queue = []
+
+
 def process_packet(packet):
-    # Aqui você pode adicionar a lógica de processamento dos pacotes capturados
-    # Neste exemplo, estamos apenas convertendo o pacote para dicionário e imprimindo
+    #print(packet.show())
+
+    if ARP in packet:
+        return
+
+    # Ethernet header
+    packet_info = {
+        "timestamp": time.time(),
+        "source_mac": packet.src,
+        "destination_mac": packet.dst,
+    }
+
+    packet = packet.payload
+
+    # IPv4/IPv6
+    packet_info["source_ip"] = packet.src
+    packet_info["destination_ip"] = packet.dst
+    packet_info["version"] = packet.version
+
+    # IPv4 has a len attribute, IPv6 has a plen attribute
+    packet_info["length"] = packet.len if packet.version == 4 else packet.plen
+
+    if packet.version == 6:
+        packet_info["protocol"] = packet.nh
+
+    packet = packet.payload
+
+    # UDP/TCP/ICMP
+
+    if not "protocol" in packet_info:
+        packet_info["protocol"] = packet.name
+
+    if hasattr(packet, "sport") and hasattr(packet, "dport"):
+        packet_info["source_port"] = packet.sport
+        packet_info["destination_port"] = packet.dport
 
     if TCP in packet:
-        packet_info = {
-            "source_ip": packet[IP].src,
-            "destination_ip": packet[IP].dst,
-            "protocol": packet[IP].proto,
-            "source_port": packet[TCP].sport if TCP in packet else None,
-            "destination_port": packet[TCP].dport if TCP in packet else None,
-        }
+        packet_info["flags"] = [flag for flag in packet.flags]
 
-        # Converter para JSON e salvar em arquivo
-        with open("captured_packets.json", "a") as f:
-            json.dump(packet_info, f)
-            f.write(",\n")  # Nova linha entre os pacotes
+    queue.append(packet_info)
+    save_packet(queue)
 
 
-# Função para capturar os pacotes de rede
+def save_packet(queue):
+    if len(queue) == 10:
+        db[collection].insert_many(queue)
+        queue.clear()
+
+
 def capture_packets():
-    # Definindo a interface de rede para captura
-    interface = "Ethernet"  # Substitua "eth0" pela interface de rede correta
+
+    interface = os.getenv('NETWORK_INTERFACE')
 
     # Iniciando a captura de pacotes
     sniff(iface=interface, prn=process_packet, store=False)
+
 
 # Executando a captura de pacotes
 capture_packets()
