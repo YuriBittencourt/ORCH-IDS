d888d40e8427156c15c242d0aa5bd251372afa90
YuriBittencourt <bittencourt.yuri@gmail.com>
2023-06-08 03:32:00 -0300

    feat: adicionado regras

--- app/app.py
+++ app/app.py
@@ -49,7 +49,18 @@ def logout():
 @authenticated_resource
 def rules():
     if request.method == 'POST':
-        print(request.form)
+        try:
+            new = {k: v if schemas['rules']['properties'][k]['type'] == 'string'
+            else int(v) if schemas['rules']['properties'][k]['type'] == 'number'
+            else bool(v) for k, v in request.form.items() if v}
+
+            if "direction" not in new:
+                new["direction"] = False
+
+            mongo.db[mongo.collections['rules']].insert_one(new)
+
+        except Exception as e:
+            print(e)
 
     if request.method == 'DELETE':
         try:
@@ -75,7 +86,7 @@ def blacklist():
 
     if request.method == 'DELETE':
         try:
-            mongo.db[mongo.collections['blacklist']].delete_one({ "_id": ObjectId(request.json["_id"])})
+            mongo.db[mongo.collections['blacklist']].delete_one({"_id": ObjectId(request.json["_id"])})
         except Exception as e:
             print(e)
 
@@ -122,8 +133,8 @@ if __name__ == '__main__':
 
     elif config['ENVIRONMENT'] == 'PROD':
         from waitress import serve
+
         serve(app, host="0.0.0.0", port=config['PORT'])
 
     else:
         raise ValueError('WRONG environment variable')
-
