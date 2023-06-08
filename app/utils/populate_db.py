7fedfaafcc15453784d35fe2e14cf5ffa02b07cc
YuriBittencourt <bittencourt.yuri@gmail.com>
2023-06-08 04:48:17 -0300

    refact: fix e troca de aspas

--- app/utils/populate_db.py
+++ app/utils/populate_db.py
@@ -3,10 +3,10 @@ from .mongo import mongo_instance as mongo
 
 def blacklist():
     documents =[
-        {"ip": "192.164.0.32", "version": 4, "reason": "botnet"},
-        {"ip": "192.164.2.32", "version": 4, "reason": "suspicious"},
-        {"ip": "2001:0db8:85a3:0000:0000:8a2e:0370:7334", "version": 6, "reason": "botnet"},
-        {"ip": "2001:0db8:85a3:0000:0000:8a2e:0370:7335", "version": 6, "reason": "suspicious"},
+        {'ip': '192.164.0.32', 'ip_version': 4, 'reason': 'botnet'},
+        {'ip': '192.164.2.32', 'ip_version': 4, 'reason': 'suspicious'},
+        {'ip': '2001:0db8:85a3:0000:0000:8a2e:0370:7334', 'ip_version': 6, 'reason': 'botnet'},
+        {'ip': '2001:0db8:85a3:0000:0000:8a2e:0370:7335', 'ip_version': 6, 'reason': 'suspicious'},
     ]
     mongo.db[mongo.collections['blacklist']].delete_many({})
     mongo.db[mongo.collections['blacklist']].insert_many(documents, ordered=False)
@@ -15,70 +15,70 @@ def blacklist():
 def packets():
     documents =[
         {
-            "timestamp": 1686044761376,
-            "source_ip": "192.168.0.114",
-            "destination_ip": "66.22.246.136",
-            "version": 4,
-            "length": 300,
-            "protocol": "UDP",
-            "source_port": 54380,
-            "destination_port": 50002
+            'timestamp': 1686044761376,
+            'source_ip': '192.168.0.114',
+            'destination_ip': '66.22.246.136',
+            'ip_version': 4,
+            'length': 300,
+            'protocol': 'UDP',
+            'source_port': 54380,
+            'destination_port': 50002
         },
         {
-            "timestamp": 1686044761380,
-            "source_ip": "192.168.0.114",
-            "destination_ip": "66.22.246.136",
-            "version": 4,
-            "length": 300,
-            "protocol": "UDP",
-            "source_port": 54380,
-            "destination_port": 50002
+            'timestamp': 1686044761380,
+            'source_ip': '192.168.0.114',
+            'destination_ip': '66.22.246.136',
+            'ip_version': 4,
+            'length': 300,
+            'protocol': 'UDP',
+            'source_port': 54380,
+            'destination_port': 50002
         },
         {
-            "timestamp": 1686044761381,
-            "source_ip": "192.168.0.114",
-            "destination_ip": "66.22.246.136",
-            "version": 4,
-            "length": 300,
-            "protocol": "UDP",
-            "source_port": 54380,
-            "destination_port": 50002
+            'timestamp': 1686044761381,
+            'source_ip': '192.168.0.114',
+            'destination_ip': '66.22.246.136',
+            'ip_version': 4,
+            'length': 300,
+            'protocol': 'UDP',
+            'source_port': 54380,
+            'destination_port': 50002
         },
         {
-            "timestamp": 1686044761434,
-            "source_ip": "192.168.0.114",
-            "destination_ip": "162.159.129.235",
-            "version": 4,
-            "length": 127,
-            "protocol": "TCP",
-            "source_port": 58964,
-            "destination_port": 443,
-            "flags": [
-                "P",
-                "A"
+            'timestamp': 1686044761434,
+            'source_ip': '192.168.0.114',
+            'destination_ip': '162.159.129.235',
+            'ip_version': 4,
+            'length': 127,
+            'protocol': 'TCP',
+            'source_port': 58964,
+            'destination_port': 443,
+            'flags': [
+                'P',
+                'A'
             ]
         },
         {
-            "timestamp": 1686044761447,
-            "source_ip": "192.168.0.114",
-            "destination_ip": "35.215.218.63",
-            "version": 4,
-            "length": 71,
-            "protocol": "UDP",
-            "source_port": 55612,
-            "destination_port": 50002
+            'timestamp': 1686044761447,
+            'source_ip': '192.168.0.114',
+            'destination_ip': '35.215.218.63',
+            'ip_version': 4,
+            'length': 71,
+            'protocol': 'UDP',
+            'source_port': 55612,
+            'destination_port': 50002
         },
         {
-            "timestamp": 1686044761452,
-            "source_ip": "162.159.129.235",
-            "destination_ip": "192.168.0.114",
-            "version": 4,
-            "length": 40,
-            "protocol": "TCP",
-            "source_port": 443,
-            "destination_port": 58964,
-            "flags": [
-                "A"
+            'timestamp': 1686044761452,
+            'source_ip': '162.159.129.235',
+            'destination_ip': '192.168.0.114',
+            'ip_version': 4,
+            'length': 40,
+            'protocol': 'TCP',
+            'source_port': 443,
+            'destination_port': 58964,
+            'flags': [
+                'A'
             ]
         }
     ]
@@ -89,18 +89,18 @@ def packets():
 def rules():
     documents = [
         {
-            'name': "PING",
-            'description': "someone pinged the host 192.168.0.124",
+            'name': 'PING',
+            'description': 'someone pinged the host 192.168.0.124',
             'severity':  2,
             'direction': True,
-            'source_ip': "177.230.25.122",
+            'source_ip': '177.230.25.122',
             'destination_ip': '192.168.0.124',
             'ip_version': 4,
             'protocol': 'ICMP',
         },
         {
-            'name': "PortScan",
-            'description': "someone portscanned the host 192.168.0.120",
+            'name': 'PortScan',
+            'description': 'someone portscanned the host 192.168.0.120',
             'severity': 2,
             'direction': True,
             'destination_ip': '192.168.0.120',
@@ -118,7 +118,7 @@ def rules():
 def alerts():
     documents = [
         {
-            'name': "PING",
+            'name': 'PING',
             'severity': 2,
             'timestamp': 1686044761376,
             'protocol': 'ICMP',
@@ -127,7 +127,7 @@ def alerts():
         },
 
         {
-            'name': "PortScan",
+            'name': 'PortScan',
             'severity': 2,
             'timestamp': 1686044761377,
             'protocol': 'TCP',
